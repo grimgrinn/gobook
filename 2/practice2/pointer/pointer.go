df043d9f2b8ccfe278ba586244b809e0520036de
grimgrinn <grimgrinn@gmail.com>
2026-02-22 04:41:54 +0300

    2.3.2

---
+++ 2/practice2/pointer/pointer.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var p = f()
+
+	fmt.Println(p)
+	p = f()
+	fmt.Println(p)
+	p = f()
+	fmt.Println(p)
+	p = f()
+	fmt.Println(p)
+}
+
+func f() *int {
+	v := 1
+	return &v
+}
