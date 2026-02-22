d635467ab55d8b6e9c5d868d6ac8b0c1ad4a46f1
grimgrinn <grimgrinn@gmail.com>
2026-02-22 15:32:18 +0300

    2.6 practice

--- 2/tempconv/conv.go
+++ 2/tempconv/conv.go
@@ -6,8 +6,14 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC преобразует температуру по Фаренгейту в температуру по Цельсию.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
-// CtoK преобразует темпертаруту по Цельсию в температуру по Кельвину.
-func CtoK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+// CToK преобразует темпертаруту по Цельсию в температуру по Кельвину.
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
 
-// KtoC преобразует температуру по Келвину в температуру по Цельсию.
-func KtoC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+// KToC преобразует температуру по Келвину в температуру по Цельсию.
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// FToK преобразует температуру по Фаренгейту в температуру по Кельвину.
+func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
+
+// KToF преобразует температуру по Кельвину в температуру по Фаренгейту.
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(CToF(KToC(k))) }
