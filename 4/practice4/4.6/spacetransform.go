28c0b74fab516f32b4035d62ee40c7a6952e1957
grimgrinn <grimgrinn@gmail.com>
2026-03-19 06:41:31 +0300

    practice 4.6

---
+++ 4/practice4/4.6/spacetransform.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func spaceTransform(s string) string {
+
+	if len(s) <= 1 {
+		return s
+	}
+
+	write := 1
+
+	str := []rune(s)
+	space := false
+
+	if unicode.IsSpace(str[0]) {
+		str[0] = ' '
+		space = true
+	}
+
+	for read := 1; read < len(str); read++ {
+		if unicode.IsSpace(str[read]) {
+			if !space {
+				str[write] = ' '
+				write++
+				space = true
+			}
+			// если не пробел пропускаем
+		} else {
+			str[write] = str[read]
+			write++
+			space = false
+		}
+	}
+	return string(str[:write])
+}
+
+func main() {
+	str := "what a fuck pepe    shneyne  pepe wtfaa   wfaa pepe sneyne"
+
+	fmt.Println(str)
+
+	str = spaceTransform(str)
+
+	fmt.Println(str)
+
+}
