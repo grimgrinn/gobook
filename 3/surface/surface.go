80bece4597864407b97cea88609fdc7ec1ad75a9
grimgrinn <grimgrinn@gmail.com>
2026-04-11 00:44:25 +0300

    5 practice 5.6

--- 3/surface/surface.go
+++ 3/surface/surface.go
@@ -34,16 +34,16 @@ func main() {
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (sx, sy float64) {
 	// Ищем угловую точку (x,y) ячейки (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 	// Вычисляем высоту поверхности z
 	z := f(x, y)
 	// Изометрически проецируем (x,y,z) на двумерную канву SVG (sx, sy)
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	sx = width/2 + (x-y)*cos30*xyscale
+	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
+	return
 }
 
 func f(x, y float64) float64 {
