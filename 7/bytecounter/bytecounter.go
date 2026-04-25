746df9bca42e1f0fc00ccc91606731a4318239ba
grimgrinn <grimgrinn@gmail.com>
2026-04-25 17:03:57 +0300

    7 bytecounter

---
+++ 7/bytecounter/bytecounter.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c) // "5", =len("hello")
+	c = 0          // Сброс счетчика
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c) // "12", = len("hello, Dolly")
+}
