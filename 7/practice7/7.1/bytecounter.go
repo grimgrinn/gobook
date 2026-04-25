e084163293c54f392568beb32dedf99150f5a0d9
grimgrinn <grimgrinn@gmail.com>
2026-04-26 01:23:52 +0300

    practice7 7.1

--- 7/practice7/7.1/bytecounter.go
+++ 7/practice7/7.1/bytecounter.go
@@ -7,6 +7,18 @@ import (
 )
 
 type WordCounter int
+type LineCounter int
+
+func (l *LineCounter) Write(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
+	scanner.Split(bufio.ScanLines)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	*l += LineCounter(count)
+	return len(p), nil
+}
 
 func (w *WordCounter) Write(p []byte) (int, error) {
 	scanner := bufio.NewScanner(bytes.NewReader(p))
@@ -21,7 +33,13 @@ func (w *WordCounter) Write(p []byte) (int, error) {
 
 func main() {
 	var w WordCounter
+
 	w.Write([]byte("hello world"))
 	w.Write([]byte("foo bar baz"))
 	fmt.Println(w) // 5
+
+	var l LineCounter
+
+	l.Write([]byte("line1 \n line2 \n line3 \n line4 "))
+	fmt.Println(l)
 }
