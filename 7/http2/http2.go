ba46ba0a404d4be6a1013b2922695c1de0b6c363
grimgrinn <grimgrinn@gmail.com>
2026-05-01 05:29:54 +0300

    7 http1234

---
+++ 7/http2/http2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound) // 404
+			fmt.Fprintf(w, "нет товара: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+
+	default:
+		w.WriteHeader(http.StatusNotFound) // 404
+		//	fmt.Fprintf(w, "нет страницы: %s\n", req.URL)
+
+		msg := fmt.Sprintf("нет страницы: %s\n", req.URL)
+		http.Error(w, msg, http.StatusNotFound) // 404
+	}
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	log.Fatal(http.ListenAndServe("localhost:8000", db))
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
