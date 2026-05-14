6ea18b3a571e12697bed5ca4209bd103c2667116
grimgrinn <grimgrinn@gmail.com>
2026-05-14 16:01:40 +0300

    practice7 7.16

--- 7/practice7/7.16/calc.go
+++ 7/practice7/7.16/calc.go
@@ -3,56 +3,90 @@ package main
 import (
 	"fmt"
 	"gobook/7/eval"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
-	"text/template"
 )
 
+var tmpl *template.Template
+
+func init() {
+	// Загружаем щаблон при старте
+	tmpl = template.Must(template.ParseFiles("templates/calc.html"))
+}
+
 func calcHandler(w http.ResponseWriter, r *http.Request) {
+	// Данные для шаблона
+	data := struct {
+		Expr   string
+		Vars   []string    // список имен переменных
+		Result interface{} //результат (число или ошибка)
+		Error  string
+	}{}
+
+	// Получаем выражение
 	exprStr := r.FormValue("expr")
+	data.Expr = exprStr
+
 	if exprStr == "" {
-		render(w, "", nil, nil)
+		// ПОказываем пустую форму
+		tmpl.Execute(w, data)
 		return
 	}
 
+	// Парсим выражение
 	expr, err := eval.Parse(exprStr)
 	if err != nil {
-		render(w, exprStr, nil, fmt.Errorf("ошибка парсинга: %v", err))
+		data.Error = fmt.Sprintf("Ошибка парсинга: %v", err)
+		tmpl.Execute(w, data)
 		return
 	}
 
-	vars := make(map[eval.Var]bool)
-	if err := expr.Check(vars); err != nil {
-		render(w, exprStr, nil, fmt.Errorf("ошибка проверки: %v", err))
+	// Находим переменные
+	varsMap := make(map[eval.Var]bool)
+	if err := expr.Check(varsMap); err != nil {
+		data.Error = fmt.Sprintf("Ошибка проверки: %v", err)
+		tmpl.Execute(w, data)
 		return
 	}
 
+	// Преобразуем карту в срез для шаблона
+	for v := range varsMap {
+		data.Vars = append(data.Vars, string(v))
+	}
+
+	// Если это GET-запрос (первая загрузка страницы) - показываем форму с переменными
+	if r.Method == http.MethodGet {
+		tmpl.Execute(w, data)
+		return
+	}
+
+	// POST-запрос: вычисляем выражение
 	env := make(eval.Env)
-	for v := range vars {
-		val, err := strconv.ParseFloat(r.FormValue(string(v)), 64)
-		if err != nil {
-			render(w, exprStr, vars, fmt.Errorf("неверное значение для %s", v))
+	for _, v := range data.Vars {
+		valStr := r.FormValue(v)
+		if valStr == "" {
+			data.Error = fmt.Sprintf("Переменная %s не заполнена", v)
+			tmpl.Execute(w, data)
 			return
 		}
-		env[v] = val
+		val, err := strconv.ParseFloat(valStr, 64)
+		if err != nil {
+			data.Error = fmt.Sprintf("Неверное число для %s:%s", v, valStr)
+			tmpl.Execute(w, data)
+			return
+		}
+		env[eval.Var(v)] = val
 	}
 
 	result := expr.Eval(env)
-	render(w, exprStr, vars, result)
-}
-
-func render(w http.ResponseWriter, expr string, vars map[eval.Var]bool, data interface{}) {
-	tmpl := template.Must(template.ParseFiles("templates/calc.html"))
-	tmpl.Execute(w, map[string]interface{}{
-		"Expr":   expr,
-		"Vars":   vars,
-		"Result": data,
-	})
-
+	data.Result = result
+	tmpl.Execute(w, data)
 }
 
 func main() {
 	http.HandleFunc("/", calcHandler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Println("Сервер запущен на http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
