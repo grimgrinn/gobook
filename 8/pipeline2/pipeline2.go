67c4b96e173d4f4cc7d43c306b887a7c5ca00679
grimgrinn <grimgrinn@gmail.com>
2026-06-02 05:17:10 +0300

    8 pipeline2

--- 8/pipeline2/pipeline2.go
+++ 8/pipeline2/pipeline2.go
@@ -3,31 +3,27 @@ package main
 import "fmt"
 
 func main() {
-
 	naturals := make(chan int)
 	squares := make(chan int)
 
 	// Генерация
 	go func() {
-		for x := 0; ; x++ {
+		for x := 0; x < 100; x++ {
 			naturals <- x
 		}
+		close(naturals)
 	}()
 
 	// Возведение в квадрат
 	go func() {
-		for {
-			x, ok := <-naturals
-			if !ok {
-				break // Канал закрыт и опустошен
-			}
+		for x := range naturals {
 			squares <- x * x
 		}
 		close(squares)
 	}()
 
 	// Вывод (в главной go-подпрограмме)
-	for {
-		fmt.Println(<-squares)
+	for x := range squares {
+		fmt.Println(x)
 	}
 }
